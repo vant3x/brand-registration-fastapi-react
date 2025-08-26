962718536d941d92dd47cfe8296b07b7c55bb5c0
alevante <alejandrovelasquezmolina@gmail.com>
2025-08-26 00:03:18 -0500

    feat: cambiando url del backend

--- brand-frontend/next.config.ts
+++ brand-frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    api_url:'http://localhost:8000'
+    api_url:'http://api.alevante.xyz:8000'
   }
 };
 
